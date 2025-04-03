1b03b1197765273a763042f5364ed65b97515f4a
renu <renu.gusain05@gmail.com>
2025-04-04 00:29:13 +0530

    187. Repeated DNA Sequences

---
+++ src/main/java/renu/gusain/sliding/window/LeetCode187.java
@@ -0,0 +1,40 @@
+package renu.gusain.sliding.window;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class LeetCode187 {
+    public static void main(String[] args) {
+        String s = "AAAAAAAAAAA";
+        List<String> result = findRepeatedDnaSequences(s);
+        result.forEach(e -> System.out.println(e + ", "));
+    }
+
+    public static List<String> findRepeatedDnaSequences(String s) {
+
+        HashSet<String> info = new HashSet<>();
+        HashSet<String> result = new HashSet<>();
+        final int len = s.length();
+        if (len < 10) return new ArrayList<>();
+
+        info.add(s.substring(0, 10));
+        int i = 1, j = 10;
+
+        while (j < len) {
+            String currentString = s.substring(i, j + 1);
+            if (info.contains(currentString)) {
+                result.add(currentString);
+
+            } else {
+                info.add(currentString);
+            }
+            j++;
+            i++;
+
+
+        }
+        return new ArrayList<>(result);
+    }
+
+}
