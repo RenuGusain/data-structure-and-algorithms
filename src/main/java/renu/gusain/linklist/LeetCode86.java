2c25a5541b230f82db4461d835523463b34e3a19
renu <renu.gusain05@gmail.com>
2025-04-16 09:22:58 +0530

    leet code 86

---
+++ src/main/java/renu/gusain/linklist/LeetCode86.java
@@ -0,0 +1,31 @@
+package renu.gusain.linklist;
+
+public class LeetCode86 {
+    public ListNode partition(ListNode head, int x) {
+
+            ListNode beforeHead = new ListNode(0); // Dummy node for less-than-x list
+            ListNode afterHead = new ListNode(0);  // Dummy node for greater/equal list
+
+            ListNode before = beforeHead;
+            ListNode after = afterHead;
+
+            while (head != null) {
+                if (head.val < x) {
+                    before.next = head;
+                    before = before.next;
+                } else {
+                    after.next = head;
+                    after = after.next;
+                }
+                head = head.next;
+            }
+
+            after.next = null;        // End of after list
+            before.next = afterHead.next;  // Link two partitions
+
+            return beforeHead.next;
+        }
+
+    }
+
+}
---
+++ src/main/java/renu/gusain/linklist/ListNode.java
@@ -0,0 +1,18 @@
+package renu.gusain.linklist;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
