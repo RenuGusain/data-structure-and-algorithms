d1c2e9e46c7a8a73ec9755c6218bb112cc01990c
renu <renu.gusain05@gmail.com>
2025-05-08 23:10:20 +0530

    leet code

--- src/main/java/renu/gusain/array/LeetCode15.java
+++ src/main/java/renu/gusain/array/LeetCode15.java
@@ -1,32 +1,35 @@
 package renu.gusain.array;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class LeetCode15 {
     public List<List<Integer>> threeSum(int[] nums) {
-
-
         int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
+        boolean exit = false;
         for (int i = 0; i < n; i++) {
-            if(nums[i]>0)break;
-            for (int j = i + 1; j < n; j++) {
-                if(nums[i]+nums[j]>0)break;
-                for (int k = j + 1; k < n; k++) {
-                    if(nums[i]+nums[j]+nums[k]>0)break;
-                    if (i > 0 && nums[i - 1] == nums[i]) continue;
-                    if (j > i + 1 && nums[j - 1] == nums[j]) continue;
-                    if (k > j + 1 && nums[k - 1] == nums[k]) continue;
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        result.add(List.of(nums[i], nums[j], nums[k]));
-                    }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    result.add(List.of(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
                 }
+                if (sum < 0) {
+                    j++;
+                }
+                if (sum > 0) {
+                    k--;
+                }
+
             }
         }
-        return result;
-
+        return new ArrayList<>(result);
     }
+
 }
